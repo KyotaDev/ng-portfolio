144ed7ed9379c5fa6bf85f7284ae002ccba6392c
KyotaDev <kyotadeveloper@gmail.com>
2025-01-19 13:13:44 -0500

    Add: add header and main structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './features/header/header.component';
+import { MainComponent } from './features/main/main.component';
+import { AboutComponent } from './features/about/about.component';
+import { ExperienceComponent } from './features/experience/experience.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { SkillsComponent } from './features/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
